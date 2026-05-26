87ca21b6ded2a0f5be6c9779f4143517ad559480
Aki Ristkari <aki@ristkari.dev>
2026-05-26 19:31:33 +0300

    chore: scaffold lessons/06-structs

---
+++ lessons/06-structs/exercises/src/lib.rs
@@ -0,0 +1,7 @@
+//! Exercises for lesson 06-structs.
+//! Make the tests in `tests/exercise.rs` pass.
+
+#[must_use]
+pub fn add(_a: i32, _b: i32) -> i32 {
+    todo!("implement add")
+}
---
+++ lessons/06-structs/solutions/src/lib.rs
@@ -0,0 +1,6 @@
+//! Reference solution for lesson 06-structs.
+
+#[must_use]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
