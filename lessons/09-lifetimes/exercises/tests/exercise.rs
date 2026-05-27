11c6998746e6c861e116ea9c913ac5fed936d0ff
Aki Ristkari <aki@ristkari.dev>
2026-05-27 13:58:55 +0300

    chore: scaffold lessons/09-lifetimes

---
+++ lessons/09-lifetimes/exercises/tests/exercise.rs
@@ -0,0 +1,6 @@
+use lifetimes_exercises::add;
+
+#[test]
+fn adds_positives() {
+    assert_eq!(add(2, 3), 5);
+}
---
+++ lessons/09-lifetimes/solutions/tests/exercise.rs
@@ -0,0 +1,6 @@
+use lifetimes_solutions::add;
+
+#[test]
+fn adds_positives() {
+    assert_eq!(add(2, 3), 5);
+}
